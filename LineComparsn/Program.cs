6b22962a851ec624c18c97c4bc9e4f9b78acd859
Rumana Parven <rumanaparven16@gmail.com>
2020-09-27 11:10:09 -0700

    first commit

---
+++ LineComparsn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparsn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
