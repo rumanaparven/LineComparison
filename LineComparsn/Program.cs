7dc413d737c982d42e35f8b8a2aa2b7b5bd87e17
Rumana Parven <rumanaparven16@gmail.com>
2020-09-27 11:26:36 -0700

    [Rumana] Add. Calculation of length of line.

--- LineComparsn/Program.cs
+++ LineComparsn/Program.cs
@@ -6,7 +6,15 @@ namespace LineComparsn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            int x1, x2, y1, y2;
+            Random radom = new Random();
+            x1 = radom.Next(-100, 100);
+            x2 = radom.Next(-100, 100);
+            y1 = radom.Next(-100, 100);
+            y2 = radom.Next(-100, 100);
+            Console.WriteLine("The coordinates are : " + x1 + " " + y1 + " " + x2 + " " + y2);
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of the line : " + Math.Round(length, 2));
         }
     }
 }
