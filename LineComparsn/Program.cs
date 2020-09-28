5f829be7a1632d217208cb3a43388db64a8d7480
Rumana Parven <rumanaparven16@gmail.com>
2020-09-27 21:25:29 -0700

    [Rumana] Add. Check if two lines are equal

--- LineComparsn/Program.cs
+++ LineComparsn/Program.cs
@@ -1,20 +1,39 @@
 ﻿using System;
+using System.Threading;
 
 namespace LineComparsn
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            int x1, x2, y1, y2;
+
+
+            double[] length = new double[2];
             Random radom = new Random();
-            x1 = radom.Next(-100, 100);
-            x2 = radom.Next(-100, 100);
-            y1 = radom.Next(-100, 100);
-            y2 = radom.Next(-100, 100);
-            Console.WriteLine("The coordinates are : " + x1 + " " + y1 + " " + x2 + " " + y2);
-            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("Length of the line : " + Math.Round(length, 2));
+            int count = 0;
+            
+            while(count<2)
+            {
+                int x1, x2, y1, y2;
+                x1 = radom.Next(-100, 100);
+                x2 = radom.Next(-100, 100);
+                y1 = radom.Next(-100, 100);
+                y2 = radom.Next(-100, 100);
+                Console.WriteLine("The coordinates are : (" + x1 + " " + y1 + ") (" + x2 + " " + y2+")");
+                length[count] = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                Console.WriteLine("Length of the line : " + Math.Round(length[count], 2));
+                count++;
+            }
+            if (length[0]==length[1]) {
+                Console.WriteLine("Lines are equal.");
+            }
+
+            else
+            {
+                Console.WriteLine("Lines are not equal.");
+            }
         }
     }
 }
