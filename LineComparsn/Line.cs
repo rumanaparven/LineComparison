a9b12c8b1e160e9af821cdcb1f9c7cb0b3a9172f
Rumana Parven <rumanaparven16@gmail.com>
2020-09-30 13:12:41 -0700

    [Rumana] Add. Line comparison using Oops concept.

---
+++ LineComparsn/Line.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparsn
+{
+    class Line
+    {
+        public void Comparison()
+        {
+            double[] length = new double[2];
+            Random radom = new Random();
+            int count = 0;
+
+            while (count < 2)
+            {
+                int x1, x2, y1, y2;
+                x1 = radom.Next(-100, 100);
+                x2 = radom.Next(-100, 100);
+                y1 = radom.Next(-100, 100);
+                y2 = radom.Next(-100, 100);
+                Console.WriteLine("The coordinates are : (" + x1 + " " + y1 + ") (" + x2 + " " + y2 + ")");
+                length[count] = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                Console.WriteLine("Length of the line : " + Math.Round(length[count], 2));
+                count++;
+            }
+
+            int result = length[0].CompareTo(length[1]);
+
+            if (result < 0)
+            {
+                Console.WriteLine("Line 1 is smaller than line 2.");
+            }
+            else if (result == 0)
+            {
+                Console.WriteLine("Lines are equal.");
+            }
+            else
+            {
+                Console.WriteLine("Line 1 is greater than line 2.");
+            }
+        }
+    }
+}
